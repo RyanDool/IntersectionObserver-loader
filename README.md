69bcebbbb2eff5568ae4aa25c36c7792e75efcde
rldool <ryanldool@gmail.com>
2018-05-10 14:23:21 -0700

    initial push

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-IntersectionObserver-loader
+# IntersectionObserver-loader
+Helpful script to lazy-load images.
+
+## Getting Started
+Install the script onto your site.
+Add a class of "lazyloadimg" to all images and elements with background images you would like to lazy load. 
+For image elements replace src with "data-lzimg", the result should look something like this:
+```
+<img data-lzimg="/dir/someimage.jpg" alt=" " class="lazyloadimg">
+```
+
+For CSS background images add a data attribute of "lzbgimg" to the element and remove any CSS rules that apply a background image, the result should look something like this:
+```
+<div class="lazyloadimg" data-lzbgimg="/dir/someimage.jpg"></div>
+```
+
+
+## Authors
+Ryan Dool
---
+++ observer-lazyloader.js
@@ -0,0 +1,34 @@
+function loadTheImgs(e){
+	if(e.hasAttribute('data-lzbgimg')){
+		let bgImg = e.dataset.lzbgimg;
+		e.style.backgroundImage = 'url(' + bgImg + ')';
+	}
+	if(e.hasAttribute('data-lzimg')){
+		e.setAttribute('src', lazy.getAttribute('data-lzimg'));
+	}
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+	const obsrvConfig = {
+		rootMargin: '50px 0px',
+		threshold: 0.01
+	};
+	//get images wish to be lazyloaded
+	const lazyImages = document.querySelectorAll('.lazyloadimg');
+	//make sure they are really there
+	if(lazyImages.length > 0){
+		//if IntersectionObserver not supported load all images
+		if(!window.IntersectionObserver){
+			lazyImages.forEach(lazyImage => loadTheImgs(lazyImage));
+		}else{
+			let observer = new IntersectionObserver((entries) => {
+				entries.forEach(entry => {
+					if(entry.intersectionRatio > 0){
+						loadTheImgs(entry.target)
+					}
+				})
+			}, obsrvConfig);
+			lazyImages.forEach(lazyImage => {observer.observe(lazyImage)});
+		}
+	}
+});
